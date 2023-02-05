1269301039a627bcbfc0e372732fc5d3a48b63f0
Winston Langat <winston.langat@student.moringaschool.com>
2023-02-05 21:21:56 +0300

    simple ifelse statements in python

---
+++ IfElse.py
@@ -0,0 +1,9 @@
+name = input('What is your name? ')
+Today_is_Sunday = True
+
+if Today_is_Sunday:
+    print(name.upper())
+    print(f' today is a Sunday go to church')
+else:
+    print(name.upper())
+    print(f' go to work')
