812334d7dcdc827c913cb0ec05bd3e33a9491afd
Winston Langat <winston.langat@student.moringaschool.com>
2023-02-06 09:05:00 +0300

    Iflsee

--- IfElse.py
+++ IfElse.py
@@ -1,3 +1,4 @@
+# First test
 name = input('What is your name? ')
 Today_is_Sunday = True
 
@@ -7,3 +8,34 @@ if Today_is_Sunday:
 else:
     print(name.upper())
     print(f' go to work')
+   
+#    Second test involving two conditions
+has_high_income = True
+has_good_credit = True
+
+if has_high_income and  has_good_credit:
+    print(f' You are eligible for a loan')
+else:
+    print(f' You are not eligible for a loan')
+
+#  Third test involving either
+is_a_student = False
+is_young = True
+
+if is_a_student or  is_young:
+    print(f' Ticket granted')
+else:
+    print(f' Ticket not granted')
+    
+# Fourth test involving one condition but not the other
+has_a_good_credit = False
+has_a_criminal_record = True
+
+if has_a_good_credit and not  has_a_criminal_record:
+    print(f' You can enroll')
+else:
+    print(f' You cannot enroll')
+    
+    # Fourth test involving comparison of two conditions
+    
+    
